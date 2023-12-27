b6b19a43ef75fe1f0f81d0b48231e6d0503631f7
Akash Deep <akashdeep9939@gmail.com>
2023-12-27 14:02:17 +0000

    data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3+3) // we are using node js 
+
+console.log(3+3)
+
+console.log("Akash")
+
+let name = "Akash" // string data type
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean =>  true/falue
+//null => standlone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "")
+ 
