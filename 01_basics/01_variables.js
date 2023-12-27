0d3f1d9699a4c64356172b8f6762f6180f8f6591
Akash Deep <akashdeep9939@gmail.com>
2023-12-27 14:02:00 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "Aakashdeep9939@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dhanbad" // declare variable this typpe buut bas practice 
+
+
+// accountId = 2; // Not allowed
+
+accountEmail = "ad@gmail.com";
+accountPassword = "21212121"
+accountCity = "Kolkata"
+
+console.log(accountId);
+
+/*
+Prefer not to use let not var
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
