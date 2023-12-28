a7cc7e092f3c6baa1c517724297e1ae2ab5ca017
Akash Deep <akashdeep9939@gmail.com>
2023-12-28 14:51:53 +0000

    comparsion in js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1); 
+// console.log( 2 >= 1);
+// consolelog(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1); // o/p true
+console.log("02" > 1); // o/p true
+
+console.log(null > 0); // output false
+console.log(null == 0); // output fasle
+console.log(null >= 0); // output true
+
+console.log( =undefined > 0);
+console.log(undefined == 0);  // avoid these type of conversion 
+console.log(null >= 0);
+
+// strict check === check also data type 
+// ex) console.log("2" == 2) ye data true "2" typeconversion se 2 ho jaye ga
+// laking console.log("2" === 2) ye false dega data type bhi check hoga
+
+console.log("2" === 2);
+
