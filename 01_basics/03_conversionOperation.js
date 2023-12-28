b138b8fad6ef13240ec4820d5e1a1b4b18b756ca
Akash Deep <akashdeep9939@gmail.com>
2023-12-28 14:35:40 +0000

    added fwe more conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,36 @@ let StringNumber = String(someNumber)
 console.log(StringNumber);
 console.log(typeof StringNumber);
 
+// ******************* Operation ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // 2 power 2 
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Akash"
+
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true); // output true
+console.log(+true); // output 1
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
