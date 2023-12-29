4a63b2ef4db185f38b004cbf34fa9871e7f18ea2
Akash Deep <akashdeep9939@gmail.com>
2023-12-29 13:48:01 +0000

    data types summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// there are two types of data in js premitive and non-primitive 
+// data types are how they kept in memory  and fetch it 
+
+// Primitive : there are call by value data mean change in actual data 
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Pefrence Data type (Non Primitive) => refrence creaate in memory
+// Array, Objects, Function 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123') // these two symbos is different not same
+
+console.log(id === anotherId); // output false
+
+const bigNumber = 345678969421n
+
+const heros = ["shaktiman", "naagraj", "doga"]; // this is how array declare in js 
+
+let myObj = {
+    name: "Akash", // this is a boject in js which store key value pair
+    age: 22,
+}
+
+const myFunction = function (){
+    console.log("Hello world");
+}
+
+// console.log(typeof bigNumber);
+
+console.log(typeof heros); // non primiive typeof is object 
+
+
+
+
+
