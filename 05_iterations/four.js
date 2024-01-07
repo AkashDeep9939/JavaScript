de98a57cfb17e107811cb67ab29e807d470400b3
Akash Deep <akashdeep9939@gmail.com>
2024-01-07 13:37:16 +0000

    js

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+// iteration in object using forin loop
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: 'ruby',
+//     swift: 'swift by apple'
+// }
+
+// for(const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// iteration in array using forin loop key array keys
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming){
+//     console.log(programming[key]);
+// }
+
+// blow code give nothing in output because itertion in map using forin loop not possible 
+
+// const map = new Map()
+// map.set('IN', "INDIA")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+// for(const key in map){
+//     console.log(key);
+// }
